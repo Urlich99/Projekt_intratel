9935713e2a480dc999932c63dba318c8e579dc67
asapvtec98 <108728827+asapvtec98@users.noreply.github.com>
2022-07-12 11:19:37 +0200

    Usuniecie folderu po scaleniu z backendem

--- frontend/js/date.js
+++
@@ -1,8 +0,0 @@
-`use strict`;
-function refreshTime() {
-  const timeDisplay = document.getElementById("time");
-  const dateString = new Date().toLocaleString([], {hour: '2-digit', minute:'2-digit',  second:'2-digit', hour12: false});
-  const formattedString = dateString.replace(", ", "  ");
-  timeDisplay.textContent = formattedString;
-}
-  setInterval(refreshTime, 1000);
