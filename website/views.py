e119cb3d0b6c6d4d3ddbc3144796dfe1f7fa111b
DrLaser <makskut05@gmail.com>
2022-07-18 09:42:59 +0200

    Dodalem sposob pobierania obrazu do backendu

--- website/views.py
+++ website/views.py
@@ -1,7 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
+import json
+import base64
+import io
+from PIL import Image
 
 views = Blueprint("views", __name__)
 
 @views.route("/")
 def home():
     return render_template("index.html")
+
+@views.route("/image", methods=["POST"])
+def receive_image():
+    response = jsonify({"success": True})
+    request_json = request.get_json()
+    coordinates = request_json['coordinates']
+    cropdata = (coordinates["left"], coordinates["top"], coordinates["width"],  coordinates["height"])
+    b64_im = request_json['image'].split(",")[1]
+    im = Image.open(io.BytesIO(base64.b64decode(b64_im)))
+    im = im.crop(cropdata)
+    im = im.convert('RGB')
+    im.save("test_image.jpg")
+    return response
