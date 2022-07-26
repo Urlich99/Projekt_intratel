d27d42d7f5bb2fa1c35b49d49dfbc98afa0193c0
asapvtec98 <bartosz.winkiewicz@infotech.edu.pl>
2022-07-26 14:30:42 +0200

    naprawa zaznaczania

---
+++ website/static/js/fit_canvas.js
@@ -0,0 +1,5 @@
+var clientWidth = document.getElementById('image_box').clientWidth;
+var clientHeight = document.getElementById('image_box').clientHeight;
+var canvas = document.getElementById('image_canvas');
+canvas.width = clientWidth;
+canvas.height = clientHeight;
